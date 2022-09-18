0d42493fa51b7c85e3cea83e29d40432dc9c6ccf
SandraWikstrom <wisa21rw@student.ju.se>
2022-09-18 13:33:15 +0200

    Added reviews to the about page.

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const data = require('./data.js')
 
-const app = express()
+const app = express();
 
-app.engine('hbs', expressHandlebars.engine({
-    defaultLayout: 'main.hbs',
-}))
+app.engine(
+  "hbs",
+  expressHandlebars.engine({
+    defaultLayout: "main.hbs",
+  })
+);
 
-app.use(
-    express.static('static')
-)
+app.use(express.static("static"));
 
-app.get('/', function(request, response){
-    response.render('home.hbs')
-})
+app.get("/", function (request, response) {
+  response.render("home.hbs");
+});
 
-app.listen(8080)
+app.get("/about-me", function (request, response) {
+  
+    const model = {
+    reviews: data.reviews
+  }
+
+  response.render("about.hbs", model);
+});
+
+app.listen(8080);
---
+++ data.js
@@ -0,0 +1,7 @@
+exports.reviews = [{
+    id: 1,
+    evaluation: "Great to work with, probably the best photographer in the world.",
+}, {
+    id: 2,
+    evaluation: "I have never been this impressed by someone in my entire life.",
+}]
